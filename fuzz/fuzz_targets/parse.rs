3e14893146634d7becd22a279be45f4aff739ddb
Joe Neeman <joe.neeman@tweag.io>
2024-10-18 14:42:16 +0700

    Fix lint

--- fuzz/fuzz_targets/parse.rs
+++ fuzz/fuzz_targets/parse.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        let _ = saphyr::Yaml::load_from_str(&s);
+        let _ = saphyr::Yaml::load_from_str(s);
     }
 });
