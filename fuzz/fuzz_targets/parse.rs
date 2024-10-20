9812dacc281fe09072539ab48b09c397c57e7d09
Ethiraric <ethiraric@gmail.com>
2024-10-20 16:21:25 +0200

    Fuzz with both `StrInput` and `BufferedInput`.

--- fuzz/fuzz_targets/parse.rs
+++ fuzz/fuzz_targets/parse.rs
@@ -4,6 +4,31 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
-        let _ = saphyr::Yaml::load_from_str(s);
+        let mut str_events = vec![];
+        let mut str_error = None;
+        let mut iter_events = vec![];
+        let mut iter_error = None;
+
+        for x in saphyr_parser::Parser::new_from_str(s) {
+            match x {
+                Ok(event) => str_events.push(event),
+                Err(e) => {
+                    str_error = Some(e);
+                    break;
+                }
+            }
+        }
+        for x in saphyr_parser::Parser::new_from_iter(s.chars()) {
+            match x {
+                Ok(event) => iter_events.push(event),
+                Err(e) => {
+                    iter_error = Some(e);
+                    break;
+                }
+            }
+        }
+
+        assert_eq!(str_events, iter_events);
+        assert_eq!(str_error, iter_error);
     }
 });
