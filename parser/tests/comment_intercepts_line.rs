9b1b7235413b60325eb9b33f489e947fe6e0ad33
Bourumir Wyngs <bourumir.wyngs@gmail.com>
2026-01-11 16:21:46 +0100

    More convincible test case

--- parser/tests/comment_intercepts_line.rs
+++ parser/tests/comment_intercepts_line.rs
@@ -1,9 +1,11 @@
-use saphyr_parser::{Event, Parser};
+use saphyr_parser::{Event, Parser, ScalarStyle};
 
 /// Comment intercepting the multiline text is invalid YAML (case BS4K)
 #[test]
 fn bs4k_comment_between_plain_scalar_lines_should_fail() {
-    let yaml = "word1  # comment\nword2\n";
+    let yaml = r#"word1  # comment
+word2
+"#;
 
     let mut parser = Parser::new_from_str(yaml);
     while let Some(next) = parser.next() {
@@ -20,4 +22,41 @@ fn bs4k_comment_between_plain_scalar_lines_should_fail() {
             _ => {}
         }
     }
+}
+
+#[test]
+fn bs4k_comment_between_plain_scalar_lines_in_map_should_fail() {
+    let yaml = r#"
+key: word1  # comment
+  word2
+"#;
+
+    let mut parser = Parser::new_from_str(yaml);
+    let mut got_error = false;
+    while let Some(next) = parser.next() {
+        if let Err(err) = next {
+            assert_eq!(err.info(), "comment intercepting the multiline text",
+                       "BS4K: comment intercepting the multiline text is invalid YAML in a map"
+            );
+            got_error = true;
+            break;
+        }
+    }
+    assert!(got_error, "Should have encountered BS4K error in map");
+}
+
+#[test]
+fn multiline_plain_scalar_in_map_without_comment_is_valid() {
+    let yaml = r#"
+key: word1
+  word2
+"#;
+
+    let mut parser = Parser::new_from_str(yaml);
+    let mut events = Vec::new();
+    while let Some(next) = parser.next() {
+        events.push(next.unwrap().0);
+    }
+
+    assert!(events.contains(&Event::Scalar("word1 word2".into(), ScalarStyle::Plain, 0, None)));
 }
